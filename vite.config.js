97a3da888c68351b3fc28e42021d923024e9921c
M8541 <2918382790@qq.com>
2026-03-06 22:46:08 +0800

    直接部署dist中适配好base的产物

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/zeabur-homework/'
+  base: '/zeabur-homework/' // 让所有资源路径带上仓库名，适配GitHub Pages子目录
 })
